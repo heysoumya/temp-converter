7ac57c2f5987d7dac95086af96f308042ee307b0
heysoumya <soupckm@gmail.com>
2025-07-13 17:33:00 -0700

    Working Streatlit UI

--- converter.py
+++ converter.py
@@ -1,9 +1,27 @@
+import streamlit as st
+
 def celsius_to_fahrenheit(c):
     return (c * 9/5) + 32
 
 def fahrenheit_to_celsius(f):
     return (f - 32) * 5/9
 
-if __name__ == "__main__":
-    print("32°C =", celsius_to_fahrenheit(32), "°F")
-    print("89.6°F =", fahrenheit_to_celsius(89.6), "°C")
+#--Streamlit UI--
+st.title("Temperature Converter")
+st.write("Convert between Celsius and Fahrenheit")
+
+col1, col2 = st.columns(2)
+with col1:
+    temperature = st.number_input("Enter temperature:",value=0.0)
+
+with col2:
+    unit = st.radio("Select unit",["Celsius","Fahrenheit"])
+
+#--Conversion logic--
+if unit in ["Celsius"]:
+    result = celsius_to_fahrenheit(temperature)
+    st.success(f"The temperature is: {result} F")
+
+else:
+    result = fahrenheit_to_celsius(temperature)
+    st.success(f"The temperature is: {result} C")
